ad0bad11f87bb91ed6c017ea884b4af1376d2dd1
Piotr Konopacki <konopackipio@gmail.com>
2020-11-18 16:46:21 +0100

    Added code refactorization.

--- getting_started_app/lib/main.dart
+++ getting_started_app/lib/main.dart
@@ -1,24 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:getting_started_app/location_detail.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Welcome to Flutter",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      )
-    );
+    return MaterialApp(title: "Welcome to Flutter", home: LocationDetail());
   }
 }
