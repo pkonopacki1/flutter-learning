b800e39e8e24e28089e589ae6cf2cbc566c8779f
Piotr Konopacki <konopackipio@gmail.com>
2020-11-24 13:04:00 +0100

    Added mock data

--- getting_started_app/lib/location_detail.dart
+++ getting_started_app/lib/location_detail.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:getting_started_app/models/location.dart';
 
 class LocationDetail extends StatelessWidget {
+  final Location location;
+
+  LocationDetail(this.location);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text(location.name),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            _section("red", Colors.red),
-            _section("blue", Colors.blue),
-            _section("green", Colors.green)
-          ],
+          children: _renderFacts(location)
         ));
   }
 
-  Widget _section(String title, Color color) {
-    return Container(
-      decoration: BoxDecoration(color: color),
-      child: Text(title),
-    );
+  List<Widget> _renderFacts(Location location) {
+    var result = List<Widget>();
+    for(int i = 0; i < location.facts.length; i++) {
+      result.add(_sectionTitle(location.facts[i].title));
+      result.add(_sectionText(location.facts[i].text));
+    }
+    return result;
+  }
+
+  Widget _sectionTitle(String text) {
+    return Text(text);
+  }
+
+  Widget _sectionText(String text) {
+    return Text(text);
   }
 }
--- getting_started_app/lib/main.dart
+++ getting_started_app/lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:getting_started_app/location_detail.dart';
+import 'package:getting_started_app/mocks/mock_location.dart';
+import 'package:getting_started_app/models/location.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final Location mockLockation = MockLocation.FetchAny();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "Welcome to Flutter", home: LocationDetail());
+    return MaterialApp(
+        title: "Welcome to Flutter", home: LocationDetail(mockLockation));
   }
 }
